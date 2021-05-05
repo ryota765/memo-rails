52afead9da15e24d81f26f26c2425f54b14e7f1f
Ryota Nomura <ryota.nomura@umitron.com>
2021-05-05 19:50:25 +0900

    add new function without error message

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -6,4 +6,23 @@ class MemosController < ApplicationController
   def show
     @memo = Memo.find(params[:id])
   end
+
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+
+    if @memo.save
+      redirect_to @memo
+    else
+      render :new
+    end
+  end
+
+  private
+    def memo_params
+      params.require(:memo).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "memos#index"
 
-  get "/memos", to: "memos#index"
-  get "/memos/:id", to: "memos#show"
+  resources :memos
 end
