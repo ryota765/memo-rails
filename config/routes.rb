af0a2b061f68794c14f8c680dfa1d6344c5dcc1e
Ryota Nomura <ryota.nomura@umitron.com>
2021-05-05 18:44:38 +0900

    add root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "memos#index"
+
   get "/memos", to: "memos#index"
 end
